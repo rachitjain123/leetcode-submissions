a71ea913feedbbd648a99d4a05f752cfcd8d75b3
Rachit Jain <jainrachit100@gmail.com>
2019-12-03 19:19:17 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define  pb push_back
+#define nl "\n"
+#define fastScan ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+int main()
+{
+	fastScan;
+	
+}
